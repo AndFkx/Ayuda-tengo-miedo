d7d39561f4db76e7bfc4702e53b0611aef035de4
AndFkx <134234718+AndFkx@users.noreply.github.com>
2025-04-16 19:17:36 -0500

    Add files via upload

--- api.js
+++ api.js
@@ -1,10 +1,1 @@
-async function  conexionLista() {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025')
-    const data = await res.json()
-    return data.results;
-}
-
-async function General() {
-    const infoPokes = await conexionLista();
-    mostrarLista(infoPokes) 
-}
+// api.js - Código aquí...
